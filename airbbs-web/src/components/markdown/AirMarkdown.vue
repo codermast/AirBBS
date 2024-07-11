be79020adece7b8c1c5ae6ef67f6e9f77a5fec99
codermast <codermast@qq.com>
2024-07-11 17:52:14 +0800

    Markdown Editor

--- airbbs-web/src/components/markdown/AirMarkdown.vue
+++ airbbs-web/src/components/markdown/AirMarkdown.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import Vditor from 'vditor';
 import 'vditor/dist/index.css';
 
 const vditor = ref<Vditor | null>(null);
 
-onMounted(() => {
-  vditor.value = new Vditor('vditor', {
-    after: () => {
-      // vditor.value is a instance of Vditor now and thus can be safely used here
-      vditor.value!.setValue('Vue Composition API + Vditor + TypeScript Minimal Example');
-    },
-  });
+// 接收 options 参数
+const props = defineProps<{
+  options: IOptions;
+}>();
 
+onMounted(() => {
+  vditor.value = new Vditor('vditor',
+    props.options,
+  );
 });
 </script>
 
 <template>
-  <div id="vditor" />
+  <div id="vditor"/>
 </template>
 
 <style scoped>
